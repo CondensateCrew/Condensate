9963b15c6b538749981cbd306dc2964c12ca044c
James Miller <james.rex.miller.4@gmail.com>
2020-02-17 08:43:08 -0700

    Add passing tests for change events for inputs of LoginForm

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -4,23 +4,7 @@ import './App.scss';
 const App: React.FC = () => {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <h2>Hello</h2>
->>>>>>> Rewrite App and add snapshot test
     </div>
   );
 }
--- src/Components/LoginForm/LoginForm.test.tsx
+++ src/Components/LoginForm/LoginForm.test.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import App from './App';
-import { shallow } from 'enzyme';
+import LoginForm from './LoginForm';
+import { shallow, mount } from 'enzyme';
 
-describe('App component', () => {
+describe('LoginForm component', () => {
+  let wrapper: Object;
+  beforeEach(() => {
+    wrapper = shallow(<LoginForm />);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+
   it('should match the snapshot', () => {
-    const wrapper = shallow(<App />)
     expect(wrapper).toMatchSnapshot();
-  })
+  });
+  describe('Change Events', () => {
+
+  it('should update email state when change events occur', () => {
+      const mockEvent: Object = { target: { value: 'example@email.com' } }
+      const wrapper = mount(<LoginForm />);
+      expect(wrapper.find('input').first().getDOMNode().value).toEqual('');
+      wrapper.find('input').first().simulate('change', mockEvent);
+      expect(wrapper.find('input').first().getDOMNode().value).toEqual('example@email.com');
+    });
+
+    it('should update password state when change events occur', () => {
+      const mockEvent: Object = { target: { value: 'password123' } }
+      const wrapper = mount(<LoginForm />);
+      expect(wrapper.find('input').last().getDOMNode().value).toEqual('');
+      wrapper.find('input').last().simulate('change', mockEvent);
+      expect(wrapper.find('input').last().getDOMNode().value).toEqual('password123');
+    });
+  });
 });
