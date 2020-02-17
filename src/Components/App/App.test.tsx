391ae8971a2a720d1c60103eb115086c1a653c24
Raisa Primerova <riyuhide@gmail.com>
2020-02-17 11:24:12 -0700

    Update App test

--- src/Components/App/App.test.tsx
+++ src/Components/App/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper).toMatchSnapshot();
+  })
 });
