5136f920654f8c5f9d7acd4280caf3d66d089d13
James Miller <james.rex.miller.4@gmail.com>
2020-02-17 08:43:57 -0700

    Add logic for setState for both inputs

--- src/Components/LoginForm/LoginForm.tsx
+++ src/Components/LoginForm/LoginForm.tsx
@@ -2,14 +2,22 @@ import React, { useState, } from 'react';
 import './LoginForm.scss';
 
 const LoginForm: React.FC = () => {
-  const [ email, setEmail ] = useState('');
-  const [ password, setPassword ] = useState('');
+  const [ email, setEmail ] = useState<string>('');
+  const [ password, setPassword ] = useState<string>('');
 
   return (
     <div>
-      <h2>Hello</h2>
+      <form>
+        <label htmlFor='email'>Email</label>
+        <input id='email' type='text' placeholder='name@email.com' value={email} 
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}></input>
+        <label htmlFor='password'>Password</label>
+        <input id='password' type='password' placeholder='********' value={password} 
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}></input>
+        <button type='button'>Login</button>
+      </form>
     </div>
   )
 }
 
-module.exports = LoginForm;
+export default LoginForm;
