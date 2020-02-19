23e2e687d1b8d7f8d211a5ebaa0d42f866201378
Raisa Primerova <riyuhide@gmail.com>
2020-02-19 13:07:51 -0700

    Update action creator

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -5,141 +5,161 @@ import {
   UserLoginReceived,
   RandomWordCollection,
   Insight,
-  ActionObject
-} from '../../interfaces';
+  ActionObject,
+  IAddUserAction,
+  ILogoutUserAction,
+  IAddAllActionsAction,
+  IAddNewActionAction,
+  IAddAllBrainstormsAction,
+  IAddNewBrainstormAction,
+  IDeleteBrainstormAction,
+  IAddAllCategoriesAction,
+  IAddNewCategoryAction,
+  IAddRandomWordCollectionsAction,
+  IAddQuestionTemplatesAction,
+  IAddSecretSauceAction,
+  IAddCurrentBrainstormAction,
+  IRemoveCurrentBrainstormAction,
+  IAddChosenWordAction,
+  IRemoveChosenWordAction,
+  IAddInsightAction,
+  IAddQueryAction,
+  IRemoveQueryAction,
+  IAddFilterAction,
+  IRemoveFilterAction,
+  IEndTimeAction,
+  IReverseTimeAction,
+  IEndInstructionsAction,
+  IReverseInstructionsAction
+} from 'interfaces';
 
 // user reducer action creators
-export const addUser = (user: UserLoginReceived): ActionObject => ({
+export const addUser = (user: UserLoginReceived): IAddUserAction => ({
   type: 'ADD_USER',
   user: user
 });
 
-export const logOutUser = (): ActionObject => ({
+export const logOutUser = (): ILogoutUserAction => ({
   type: 'LOG_OUT_USER',
 });
 
 // actions reducer action creators
-export const addAllActions = (actions: Action[]): ActionObject => ({
+export const addAllActions = (actions: Action[]): IAddAllActionsAction => ({
   type: 'ADD_ALL_ACTIONS',
   actions: actions
 });
 
-export const addNewAction = (action: Action): ActionObject => ({
-  type: 'ADD_NEW_ACTIONS',
+export const addNewAction = (action: Action): IAddNewActionAction => ({
+  type: 'ADD_NEW_ACTION',
   action: action
 });
 
 // allBrainstorms reducer action creators
-export const addAllBrainstorms = (brainstorms: Brainstorm[]): ActionObject => ({
+export const addAllBrainstorms = (brainstorms: Brainstorm[]): IAddAllBrainstormsAction => ({
   type: 'ADD_ALL_BRAINSTORMS',
   brainstorms: brainstorms
 });
 
-export const addNewBrainstorm = (brainstorm: Brainstorm): ActionObject => ({
+export const addNewBrainstorm = (brainstorm: Brainstorm): IAddNewBrainstormAction => ({
   type: 'ADD_BRAINSTORM',
   brainstorm: brainstorm
 });
 
-export const deleteBrainstorm = (id: number): ActionObject => ({
+export const deleteBrainstorm = (id: number): IDeleteBrainstormAction => ({
   type: 'DELETE_BRAINSTORM',
   id: id
 });
 
 // categories reducer action creators
-export const addAllCategories = (categories: Category[]): ActionObject => ({
+export const addAllCategories = (categories: Category[]): IAddAllCategoriesAction => ({
   type: 'ADD_ALL_CATEGORIES',
   categories: categories
 });
 
-export const addNewCategory = (category: Category): ActionObject => ({
+export const addNewCategory = (category: Category): IAddNewCategoryAction => ({
   type: 'ADD_CATEGORY',
   category: category
 });
 
-export const deleteCategory = (id: number): ActionObject => ({
-  type: 'DELETE_BRAINSTORM',
-  id: id
-});
-
 // randomWordCollections reducer action creators
-export const addRandomWordCollections = (collections: RandomWordCollection[]): ActionObject => ({
+export const addRandomWordCollections = (collections: RandomWordCollection[]): IAddRandomWordCollectionsAction => ({
   type: 'ADD_ALL_COLLECTIONS',
   collections: collections
 });
 
 // questionTemplates reducer action creators
-export const addQuestionTemplates = (templates: string[]): ActionObject => ({
+export const addQuestionTemplates = (templates: string[]): IAddQuestionTemplatesAction => ({
   type: 'ADD_ALL_TEMPLATES',
   templates: templates
 });
 
 // secretSauce reducer action creators
-export const addSecretSauce = (secretSauce: string[]): ActionObject => ({
-  type: 'ADD_ALL_TEMPLATES',
+export const addSecretSauce = (secretSauce: string[]): IAddSecretSauceAction => ({
+  type: 'ADD_SECRETE_SAUSE',
   secretSauce: secretSauce
 });
 
 // currentBrainstorm reducer action creators
-export const addCurrentBrainstorm = (currentBrainstorm: Brainstorm): ActionObject => ({
+export const addCurrentBrainstorm = (currentBrainstorm: Brainstorm): IAddCurrentBrainstormAction => ({
   type: 'ADD_CURRENT_BRAINSTORM',
   currentBrainstorm: currentBrainstorm
 });
 
-export const removeCurrentBrainstorm = (): ActionObject => ({
+export const removeCurrentBrainstorm = (): IRemoveCurrentBrainstormAction => ({
   type: 'REMOVE_CURRENT_BRAINSTORM'
 });
 
 // chosenWords reducer action creators
-export const addChosenWord = (chosenWord: string): ActionObject => ({
+export const addChosenWord = (chosenWord: string): IAddChosenWordAction => ({
   type: 'ADD_WORD',
   chosenWord: chosenWord
 });
 
-export const removeChosenWord = (chosenWord: string): ActionObject => ({
+export const removeChosenWord = (chosenWord: string): IRemoveChosenWordAction => ({
   type: 'REMOVE_WORD',
   chosenWord: chosenWord
 });
 
 // insights reducer action creators
-export const addInsight = (insight: Insight): ActionObject => ({
+export const addInsight = (insight: Insight): IAddInsightAction => ({
   type: 'ADD_INSIGHT',
   insight: insight
 });
 
 // query reducer action creators
-export const addQuery = (query: string): ActionObject => ({
+export const addQuery = (query: string): IAddQueryAction => ({
   type: 'ADD_QUERY',
   query: query
 });
 
-export const removeQuery = (): ActionObject => ({
+export const removeQuery = (): IRemoveQueryAction => ({
   type: 'REMOVE_QUERY'
 });
 
 // filter reducer action creators
-export const addFilter = (filter: string): ActionObject => ({
+export const addFilter = (filter: string): IAddFilterAction => ({
   type: 'ADD_FILTER',
   filter: filter
 });
 
-export const removeFilter = (): ActionObject => ({
+export const removeFilter = (): IRemoveFilterAction => ({
   type: 'REMOVE_FILTER'
 });
 
 // timeEnded reducer action creators
-export const endTime = (): ActionObject => ({
+export const endTime = (): IEndTimeAction => ({
   type: 'END_TIME'
 });
 
-export const clearTime = (): ActionObject => ({
-  type: 'CLEAR_TIME'
+export const reverseTime = (): IReverseTimeAction => ({
+  type: 'REVERSE_TIME'
 });
 
 // instructionsEnded reducer action creators
-export const endInstructions = (): ActionObject => ({
+export const endInstructions = (): IEndInstructionsAction => ({
   type: 'END_INSTRUCTIONS'
 });
 
-export const clearInstructions = (): ActionObject => ({
-  type: 'CLEAR_INSTRUCTIONS'
+export const reverseInstructions = (): IReverseInstructionsAction => ({
+  type: 'REVERSE_INSTRUCTIONS'
 });
