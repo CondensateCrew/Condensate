9881a3fe23e8bfca0c188b9b9db4e5b40219adaf
Raisa Primerova <riyuhide@gmail.com>
2020-02-19 14:38:14 -0700

    Add test for action creators connected with allBrainstorms

--- src/redux/actions/actions.test.ts
+++ src/redux/actions/actions.test.ts
@@ -64,3 +64,56 @@ describe("actions", () => {
   });
 });
 
+describe("allBrainstorms", () => {
+  it("should return object with a type of ADD_ALL_BRAINSTORMS when addAllBrainstorms is called", () => {
+    const brainstormsMock: interfaces.Brainstorm[] = [{
+      id: 1,
+      question: 'What?',
+      idea: 'Idea',
+      action: 'Build',
+      categories: [{id: 1, name: "Tech"}],
+      isGenius: true
+    }];
+
+    const expected: interfaces.IAddAllBrainstormsAction = {
+      type: 'ADD_ALL_BRAINSTORMS',
+      brainstorms: brainstormsMock
+    };
+
+    const result = actions.addAllBrainstorms(brainstormsMock)
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return object with a type of ADD_BRAINSTORM when addNewBrainstorm is called", () => {
+    const brainstormMock: interfaces.Brainstorm = {
+      id: 1,
+      question: 'What?',
+      idea: 'Idea',
+      action: 'Build',
+      categories: [{id: 1, name: "Tech"}],
+      isGenius: true
+    };
+
+    const expected: interfaces.IAddNewBrainstormAction = {
+      type: 'ADD_BRAINSTORM',
+      brainstorm: brainstormMock
+    };
+
+    const result = actions.addNewBrainstorm(brainstormMock)
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return object with a type of DELETE_BRAINSTORM when deleteBrainstorm is called", () => {
+    const expected: interfaces.IDeleteBrainstormAction = {
+      type: 'DELETE_BRAINSTORM',
+      id: 1
+    };
+
+    const result = actions.deleteBrainstorm(1)
+
+    expect(result).toEqual(expected);
+  });
+});
+
