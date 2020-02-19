6e5a0040c8855bc5b17cdfe820920d838a870bc7
Raisa Primerova <riyuhide@gmail.com>
2020-02-19 16:14:24 -0700

    Update categories reducer

--- src/redux/reducers/categories/categories.test.ts
+++ src/redux/reducers/categories/categories.test.ts
@@ -1,24 +1,29 @@
 import categoriesReducer from './categories';
-import { ActionObject, Category } from 'interfaces';
+import { ActionObject, Category, IAddAllCategoriesAction, IAddNewCategoryAction } from 'interfaces';
 
 type emptyArray = [ ];
 
 describe('categoriesReducer', () => {
   it("should return initial value", () => {
+    const mockAction: ActionObject = {
+      type: '',
+      categories: undefined
+    };
+
     const expected: emptyArray = [ ];
-    const result = categoriesReducer(undefined, {type: '', categories: undefined });
+    const result = categoriesReducer(undefined, mockAction);
 
     expect(result).toEqual(expected);
   });
 
-  it("should return the array of categories if type of action is ADD_CATEGORIES", () => {
+  it("should return the array of categories if type of action is ADD_ALL_CATEGORIES", () => {
     const mockCategories: Category[] = [{
       id: 1,
       name: 'Tech'
     }];
 
-    const mockAction: ActionObject = {
-      type: 'ADD_CATEGORIES',
+    const mockAction: IAddAllCategoriesAction = {
+      type: 'ADD_ALL_CATEGORIES',
       categories: mockCategories
     };
 
@@ -27,5 +32,23 @@ describe('categoriesReducer', () => {
     const result: Category[] = categoriesReducer(undefined, mockAction);
 
     expect(result).toEqual(expected);
-  })
+  });
+
+  it("should return the array with added category if type of action is ADD_CATEGORY", () => {
+    const mockCategory: Category = {
+      id: 1,
+      name: 'Tech'
+    };
+
+    const mockAction: IAddNewCategoryAction = {
+      type: 'ADD_CATEGORY',
+      category: mockCategory
+    };
+
+    const expected: Category[] = [ mockCategory ];
+
+    const result: Category[] = categoriesReducer([], mockAction);
+
+    expect(result).toEqual(expected);
+  });
 });
--- src/redux/reducers/categories/categories.ts
+++ src/redux/reducers/categories/categories.ts
@@ -4,8 +4,10 @@ type state = Category[ ] | [ ];
 
 const categories = (state: state = [ ], action: ActionObject) => {
   switch(action.type) {
-    case 'ADD_CATEGORIES':
+    case 'ADD_ALL_CATEGORIES':
       return action.categories;
+    case 'ADD_CATEGORY':
+      return [...state, action.category];
     default:
       return state;
   }
