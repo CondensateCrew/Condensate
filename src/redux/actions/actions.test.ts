31d0b9691ee0527e0179d7a1784378e9e6ddd80e
Raisa Primerova <riyuhide@gmail.com>
2020-02-19 14:37:27 -0700

    Add test for action creators connected with actions

--- src/redux/actions/actions.test.ts
+++ src/redux/actions/actions.test.ts
@@ -30,3 +30,37 @@ describe("user", () => {
   });
 });
 
+describe("actions", () => {
+  it("should return object with a type of ADD_ALL_ACTIONS when addAllActions is called", () => {
+    const actionsMock: interfaces.Action[] = [{
+      id: 1,
+      name: 'Build an app'
+    }];
+
+    const expected: interfaces.IAddAllActionsAction = {
+      type: 'ADD_ALL_ACTIONS',
+      actions: actionsMock
+    };
+
+    const result = actions.addAllActions(actionsMock)
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return object with a type of ADD_NEW_ACTION when addNewAction is called", () => {
+    const actionMock: interfaces.Action = {
+      id: 1,
+      name: 'Build an app'
+    };
+
+    const expected: interfaces.IAddNewActionAction = {
+      type: 'ADD_NEW_ACTION',
+      action: actionMock
+    };
+
+    const result = actions.addNewAction(actionMock)
+
+    expect(result).toEqual(expected);
+  });
+});
+
