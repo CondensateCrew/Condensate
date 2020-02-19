a0c0d81d548b0a5c56426a60c37aeb2a66be520a
Raisa Primerova <riyuhide@gmail.com>
2020-02-18 18:50:08 -0700

    Add SignUpForm component functionality

--- src/Containers/SignUpForm/SignUpForm.tsx
+++ src/Containers/SignUpForm/SignUpForm.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import './SignUpForm.scss';
+import React, { useState, useEffect } from 'react';
+import { UserSignupPosting } from '../../interfaces'
+import InputElement from '../../Components/InputElement/InputElement'
+
+type checkedInputType = 'first-name' | 'last-name' | 'email' | 'password' | 'repeat-password';
 
 interface Props {
   isLogin: boolean,
@@ -7,11 +10,63 @@ interface Props {
 }
 
 const SignUpForm: React.FC<Props> = ({ isLogin, toggleTab }) => {
+  const [ user, setUser ] = useState<UserSignupPosting>({
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+  });
+
+  const [ disabled, setDisabled ] = useState<boolean>(true);
+  const [ repeatPassword, setPassword ] = useState<string>('');
+  const [ error, setError ] = useState<string>('');
+
+  const validateButton = (): void => {
+    if (user.email === '') return setDisabled(true);
+    if (user.password === '') return setDisabled(true);
+    if (user.firstName === '') return setDisabled(true);
+    if (user.lastName === '') return setDisabled(true);
+    if (user.password !== repeatPassword) return setDisabled(true);
+    setError('')
+    return setDisabled(false);
+  }
+
+  const validateInputs = (): void => {
+    if (user.email === '') return setError('Please, add you email!');
+    if (user.password === '') return setError('Please, add you password!');
+    if (user.firstName === '') return setError('Please, add you first name!');
+    if (user.lastName === '') return setError('Please, add you last name!');
+    if (user.password !== repeatPassword) return setError('Passwords are not the same!');
+    return setError('');
+  }
+
+  useEffect(validateButton, [ user, repeatPassword ]);
+
   const submitUser = (event: React.MouseEvent): void => {
     event.preventDefault();
+    validateInputs();
+    if (error !== '') {
+      console.log(user);
+    }
   }
 
   const toggleForm = (): void => toggleTab(!isLogin);
+
+  const inputs: checkedInputType[] = ['first-name', 'last-name', 'email', 'password', 'repeat-password']
+  const inputsElements = inputs
+    .map((input: checkedInputType) => (
+      <InputElement
+        key={input}
+        typeInput={input}
+        {... {
+          user,
+          repeatPassword,
+          setPassword,
+          setUser,
+          setError
+        }}
+      />
+    ));
 
   return (
     <div className="sign-up-form">
@@ -20,42 +75,9 @@ const SignUpForm: React.FC<Props> = ({ isLogin, toggleTab }) => {
         <h2 className="hidden" onClick={toggleForm}>Login</h2>
       </header>
       <form>
-        <label htmlFor="first-name">
-          First Name*
-          <input
-            id="first-name"
-            type="text"
-            placeholder="Your First Name" />
-        </label>
-        <label htmlFor="last-name">
-          last name*
-          <input
-            id="last-name"
-            type="text"
-            placeholder="Your Last Name" />
-        </label>
-        <label htmlFor="email">
-          Email*
-          <input
-            id="email"
-            type="text"
-            placeholder="Your Email" />
-        </label>
-        <label htmlFor="password">
-          Password*
-          <input
-            id="password"
-            type="password"
-            placeholder="Your Password" />
-        </label>
-        <label htmlFor="repeat-password">
-          repeat Password*
-          <input
-            id="repeat-password"
-            type="password"
-            placeholder="Repeat Password" />
-        </label>
-        <button disabled={true} onClick={submitUser}>Sign Up</button>
+        {error !== '' && <p className="error-notification">{error}</p>}
+        {inputsElements}
+        <button disabled={disabled} onClick={submitUser}>Sign Up</button>
       </form>
     </div>
   );
