04cf15e54303c23c9ee1daadbc121de69ed22c8d
James Miller <james.rex.miller.4@gmail.com>
2020-02-20 09:39:53 -0700

    Add toggle functionality to category buttons for styles + plus button

--- src/Components/BrainstormCategory/BrainstormCategory.tsx
+++ src/Components/BrainstormCategory/BrainstormCategory.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import './BrainstromCategory.scss';
+import React, { useState, useEffect } from 'react';
+import './BrainstormCategory.scss';
 
 interface Props {
   category:string
 }
 const BrainstormCategory:React.FC<Props> = ({ category }) => {
+  const [active, setActive] = useState<boolean>(false)
+
+  const toggleActive = (e:React.MouseEvent<HTMLButtonElement>):void => {
+    setActive(!active)
+  }
+
+  let button; 
+
+  active ? button = (<button type='button' className='category-btn active' onClick={toggleActive}>{category}</button>)
+  : button = (<button type='button' className='category-btn' onClick={toggleActive}>{category}</button>)
+
   return (
-    <button className='category-btn'>{category}</button>
+    <>
+    {button}
+    </>
   )
 }
 
--- src/Containers/CategoriesField/CategoriesField.tsx
+++ src/Containers/CategoriesField/CategoriesField.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './CategoriesField.scss';
 import BrainstormCategory from '../../Components/BrainstormCategory/BrainstormCategory';
 import mockCategories from '../../data/mockCategories';
+import addIcon from '../../assets/add.svg';
 
 const CategoryField:React.FC = () => {
   const categories = mockCategories.map((category:string, index:number) => <BrainstormCategory key={index} category={category}/>)
   return (
     <section className='category-field-section'>
       {categories}
+      <img className='add-category-btn' src={addIcon}/>
     </section>
   )
 }
