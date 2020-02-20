ea2c1f3413343774fbf6bc394b483d013d136a59
Raisa Primerova <riyuhide@gmail.com>
2020-02-20 14:12:39 -0700

    Add functionality to Dashboard

--- src/Containers/Dashboard/Dashboard.tsx
+++ src/Containers/Dashboard/Dashboard.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import './Dashboard.scss';
+import { Category } from 'interfaces';
+import BrainstormContainer from '../BrainstormContainer/BrainstormContainer';
+import SearchBar from '../SearchBar/SearchBar';
+import CategoriesFilter from '../CategoriesFilter/CategoriesFilter';
+
+const categories: Category[] = [
+  {id: 1, name: 'Tech'},
+  {id: 2, name: 'Education'},
+  {id: 3, name: 'Enviroment'},
+  {id: 4, name: 'Money'},
+  {id: 5, name: 'Food'}];
 
 const Dashboard: React.FC = () => {
   return (
-    <h2>Condensate Dashboard</h2>
+    <main className="dashboard">
+      <aside>
+        <h1>condensate</h1>
+      </aside>
+      <header>
+        <CategoriesFilter {...{categories}} />
+        <SearchBar />
+      </header>
+      <BrainstormContainer />
+    </main>
+
   )
 }
 
