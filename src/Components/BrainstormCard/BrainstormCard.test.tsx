272b1cd5567867c95c329fef830221c448a5193d
Raisa Primerova <riyuhide@gmail.com>
2020-02-20 12:20:09 -0700

    Add tests to BrainstormCard component

---
+++ src/Components/BrainstormCard/BrainstormCard.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import BrainstormCard from './BrainstormCard';
+import { shallow } from 'enzyme';
+
+describe('BrainstormCard component', () => {
+  let wrapper: any;
+
+  const mockProps = {
+    id: 1,
+    question: 'What?',
+    idea: 'Idea',
+    action: 'Build an app',
+    isGenius: true,
+    categories: [{id: 1, name: 'Tech'}]
+  };
+
+  beforeEach(() => {
+    wrapper = shallow(<BrainstormCard {...mockProps}/>);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot if menu is clicked', () => {
+    wrapper.find('img[alt="menu"]').simulate('click');
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot if brainstorm is not genius', () => {
+    const wrapper = shallow(<BrainstormCard {...mockProps} isGenius={false}/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot if brainstorm is not genius and menu is clicked', () => {
+    const wrapper = shallow(<BrainstormCard {...mockProps} isGenius={false}/>);
+    wrapper.find('img[alt="menu"]').simulate('click');
+    expect(wrapper).toMatchSnapshot();
+  });
+});
