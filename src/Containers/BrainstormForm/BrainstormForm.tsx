d76abed60d17fd4803c0f9c99e6ee7c1d0085979
James Miller <james.rex.miller.4@gmail.com>
2020-02-19 20:41:30 -0700

    Add styles to BrainstormForm and add local state management for IdeaQuestion

--- src/Containers/BrainstormForm/BrainstormForm.tsx
+++ src/Containers/BrainstormForm/BrainstormForm.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './BrainstormForm.scss';
+import broom from '../../assets/broom.svg';
 import IdeaQuestion from '../../Components/IdeaQuestion/IdeaQuestion';
 import CategoriesField from '../CategoriesField/CategoriesField';
 import ActionsField from '../ActionsField/ActionsField';
 
 const BrainstormForm:React.FC = () => {
+  const [ question, setQuestion ] = useState<string>('');
+  const [ categories, setCategory ] = useState([]);
+  const [ actions, setAction ] = useState([]);
+
   return (
     <form className='brainstorm-form'>
       <h2 className='brainstorm-form-h2'>CREATE NEW BRAINSTORM</h2>
-      <label className='brainstorm-label'>IDEA QUESTION</label>
-      <IdeaQuestion />
-      <label className='brainstorm-label'>CATEGORIES</label>
-      <CategoriesField />
-      <label className='brainstorm-label'>DESIRED ACTION</label>
-      <ActionsField />
+      <div className='brainstorm-div'>
+        <label className='brainstorm-label'>IDEA QUESTION</label>
+        <IdeaQuestion setQuestion={setQuestion}/>
+      </div>
+      <div className='brainstorm-div'>
+        <label className='brainstorm-label'>CATEGORIES</label>
+        <CategoriesField />
+      </div>
+      <div className='brainstorm-div'>
+        <label className='brainstorm-label'>DESIRED ACTION</label>
+        <ActionsField />
+      </div>
+      <div className='brainstorm-div'>
+        <label className='brainstorm-label'>SUMMARY</label>
+        <textarea className='brainstorm-summary-textarea' placeholder='Typed summary here...'></textarea>
+      </div>
+      <div className='brainstorm-form-menu'>
+        <button className='cancel-btn'>cancel</button>
+        <img className='broom' src={broom}/>
+        <button className='start-btn'>start</button>
+      </div>
     </form>
   )
 }
