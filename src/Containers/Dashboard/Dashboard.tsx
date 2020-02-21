61d551d27238c4dbcfda067dd9fbed85518106fb
James Miller <james.rex.miller.4@gmail.com>
2020-02-20 21:38:31 -0700

    Resolve merge conflicts and fix stylings to incorporate changes

--- src/Containers/Dashboard/Dashboard.tsx
+++ src/Containers/Dashboard/Dashboard.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import './Dashboard.scss';
-<<<<<<< HEAD
 import { Category } from 'interfaces';
 import BrainstormContainer from '../BrainstormContainer/BrainstormContainer';
 import SearchBar from '../SearchBar/SearchBar';
 import CategoriesFilter from '../CategoriesFilter/CategoriesFilter';
+import BrainstormForm from '../BrainstormForm/BrainstormForm';
+import addIcon from '../../assets/add.svg';
 
 const categories: Category[] = [
   {id: 1, name: 'Tech'},
@@ -14,23 +15,6 @@ const categories: Category[] = [
   {id: 5, name: 'Food'}];
 
 const Dashboard: React.FC = () => {
-  return (
-    <main className="dashboard">
-      <aside>
-        <h1>condensate</h1>
-      </aside>
-      <header>
-        <CategoriesFilter {...{categories}} />
-        <SearchBar />
-      </header>
-      <BrainstormContainer />
-    </main>
-
-=======
-import BrainstormForm from '../BrainstormForm/BrainstormForm';
-import addIcon from '../../assets/add.svg';
-
-const Dashboard: React.FC = () => {
   const [ brainstormFormState, setBrainstormForm ] = useState<boolean>(false)
   let brainstormForm;
 
@@ -38,22 +22,25 @@ const Dashboard: React.FC = () => {
     setBrainstormForm(!brainstormFormState)
   }
 
-  if (brainstormFormState) {
-    brainstormForm = (<BrainstormForm brainstormFormState={brainstormFormState}
-      cancel={setBrainstormForm}/>)
-  } else {
-    brainstormForm = (
-    <div onClick={toggleMenu} className='dashboard-brainstorm-form-div'>
-      <img className='add-icon-dashboard' alt='add-icon' src={addIcon}/>
-      <h3>create new brainstorm</h3>
-    </div>
-    )}
+  brainstormFormState ? brainstormForm = (<BrainstormForm brainstormFormState={brainstormFormState}
+    cancel={setBrainstormForm}/>)
+  : brainstormForm = (<div onClick={toggleMenu} className='dashboard-brainstorm-form-div'>
+    <img className='add-icon-dashboard' alt='add-icon' src={addIcon}/>
+    <h3>create new brainstorm</h3>
+  </div>)
+
   return (
-    <main className='dashboard-main'>
-      <h2>Condensate Dashboard</h2>
+    <main className="dashboard">
+      <aside>
+        <h1>condensate</h1>
         {brainstormForm}
+      </aside>
+      <header>
+        <CategoriesFilter {...{categories}} />
+        <SearchBar />
+      </header>
+      <BrainstormContainer />
     </main>
->>>>>>> Add IdeaQuestion, CategoriesField/BSCategory, ActionsField/BSAction to BSForm
   )
 }
 
