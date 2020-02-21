5fe53b7f66054684a4ea226cc7f51ebda5ae107e
James Miller <james.rex.miller.4@gmail.com>
2020-02-20 18:46:44 -0700

    Add passing tests and snapshot for IdeaQuestion component

---
+++ src/Components/IdeaQuestion/IdeaQuestion.test.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { IBrainstormForm } from '../../interfaces';
+import IdeaQuestion from './IdeaQuestion';
+
+describe('IdeaQuestion Component', () => {
+  let wrapper;
+  beforeEach(() => {
+    interface Props {
+      formState: IBrainstormForm,
+      setQuestion: (formState: IBrainstormForm) => void
+    }
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
+  it('should match the snapshot with all the data passed through', () => {
+    let mockFormState = {
+      question: '',
+      categories: [],
+      action: 'create an app',
+      reset: true
+    };
+    let mockSetQuestion = jest.fn();
+
+    wrapper = shallow(<IdeaQuestion 
+      formState={mockFormState} 
+      setQuestion={mockSetQuestion}/>);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+  describe('handleChange', () => {
+    it('should update question when change events occur', () => {
+      const mockEvent: Object = {target: {value: 'What is love?'}} 
+      let mockFormState = {
+        question: '',
+        categories: [],
+        action: 'create an app',
+        reset: true
+      };
+      let mockSetQuestion = jest.fn();
+
+      wrapper = mount(<IdeaQuestion 
+        formState={mockFormState} 
+        setQuestion={mockSetQuestion}/>);
+
+      wrapper.find('input').simulate('change', mockEvent);
+      expect(wrapper.find('input').getDOMNode().value).toEqual('What is love?')
+    });
+  });
+});
+
+
