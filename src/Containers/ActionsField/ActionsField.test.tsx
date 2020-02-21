d61a5f413f1e3dfd26078a3b417d51c6566d85e8
James Miller <james.rex.miller.4@gmail.com>
2020-02-20 20:16:20 -0700

    Add passing tests for ActionsField + tidy up

---
+++ src/Containers/ActionsField/ActionsField.test.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { IBrainstormForm } from '../../interfaces';
+import ActionsField from './ActionsField';
+import mockActions from '../../data/mockActions';
+
+describe('ActionsField', () => {
+  let wrapper;
+  beforeEach(() => {
+    interface Props {
+      formState: IBrainstormForm,
+      setAction: (formState: IBrainstormForm) => void
+    }
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should match the snapshot', () => {
+    let mockFormState = {
+      question: '',
+      categories: [],
+      action: 'create an app',
+      reset: true
+    };
+
+    let mockSetAction = jest.fn();
+
+    wrapper = shallow(<ActionsField 
+      formState={mockFormState}
+      setAction={mockSetAction}
+    />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should call setAction when a change event occurs', () => {
+    let mockFormState = {
+      question: '',
+      categories: [],
+      action: 'create an app',
+      reset: true
+    };
+
+    let mockSetAction = jest.fn();
+
+    wrapper = mount(<ActionsField 
+      formState={mockFormState}
+      setAction={mockSetAction}
+    />);
+
+    let mockEvent = {target: {value: 'Write a Book'}};
+
+    const instance = wrapper.instance();
+
+    wrapper.find('select').simulate('change', mockEvent);
+    expect(mockSetAction).toHaveBeenCalled();
+  });
+  it('should update the local state when change event occurs', () => {
+    let mockFormState = {
+      question: '',
+      categories: [],
+      action: 'create an app',
+      reset: true
+    };
+
+    let mockSetAction = jest.fn();
+
+    wrapper = mount(<ActionsField 
+      formState={mockFormState}
+      setAction={mockSetAction}
+    />);
+
+    let mockEvent = {target: {value: 'Write a Book'}};
+    
+    expect(wrapper.find('select').getDOMNode().value).toEqual('Create an App');
+    wrapper.find('select').simulate('change', mockEvent);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
