2b7a1af7b447936af894a2acb79a8b3f7eebfd00
James Miller <james.rex.miller.4@gmail.com>
2020-02-22 08:26:08 -0700

    Add Route for RoundTwo

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.scss';
 import WelcomePage from '../WelcomePage/WelcomePage';
+<<<<<<< HEAD
 import Dashboard from 'Containers/Dashboard/Dashboard';
 import RoundOne from 'Containers/RoundOne/RoundOne';
 import RoundTwo from 'Containers/RoundTwo/RoundTwo';
+=======
+import Dashboard from '../../Containers/Dashboard/Dashboard';
+import RoundTwo from '../../Containers/RoundTwo/RoundTwo';
+
+>>>>>>> Add Route for RoundTwo
 
 const App: React.FC = () => {
   return (
@@ -12,7 +18,10 @@ const App: React.FC = () => {
       <Switch>
         <Route exact path='/' render={() => <WelcomePage />}/>
         <Route path='/dashboard' render={() => <Dashboard />}/>
+<<<<<<< HEAD
         <Route path='/gameboard/round-one' render={() => <RoundOne />}/>
+=======
+>>>>>>> Add Route for RoundTwo
         <Route path='/gameboard/round-two' render={() => <RoundTwo />}/>
         <Route component={() => <div>Not Found</div>} />
       </Switch>
