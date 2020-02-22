19c5ff9689ce73c01fe539c618514c9ffc41937e
James Miller <james.rex.miller.4@gmail.com>
2020-02-22 13:08:35 -0700

    Create GenerateInsights component and move Insight logic there

--- src/Containers/RoundTwo/RoundTwo.tsx
+++ src/Containers/RoundTwo/RoundTwo.tsx
@@ -1,7 +1,7 @@
 import React, { useState, MouseEvent, ChangeEvent, KeyboardEvent } from 'react';
 import Instruction from '../../Components/Instruction/Instruction';
 import TemplateQuestion from '../../Components/TemplateQuestion/TemplateQuestion';
-import Insights from '../Insights/Insights';
+import GenerateInsights from '../../Components/GenerateInsights/GenerateInsights';
 import Check from '../../assets/check.svg';
 import './RoundTwo.scss';
 
@@ -25,15 +25,15 @@ const RoundTwo:React.FC = () => {
   }
 
   let insights = responses.map((insight:string) => {
-    return <Insights text={insight} responses={responses} setResponses={setResponses}/>
+    return <GenerateInsights text={insight} responses={responses} setResponses={setResponses}/>
   })
 
   return (
     <section className='round-two-section'>
       <div className='instructions-div-wrapper'>
         <div className='question-numbers-div'>
-          <h2 id='one' className='question-number-h2 current-question'>1</h2>
-          <h2 id='two' className='question-number-h2'>2</h2>
+          <h2 id='one' className='question-number-h2 completed-round'>1</h2>
+          <h2 id='two' className='question-number-h2 current-round'>2</h2>
           <h2 id='three' className='question-number-h2'>3</h2>
         </div>
       <Instruction/>
