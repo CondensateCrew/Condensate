2a1f8734a417b34ba639dc0f5b9cb702102f943a
Raisa Primerova <riyuhide@gmail.com>
2020-02-23 13:05:01 -0700

    Update round one tests

--- src/Containers/RoundOne/RoundOne.test.tsx
+++ src/Containers/RoundOne/RoundOne.test.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import RoundOne from './RoundOne';
 import { shallow } from 'enzyme';
-import { useSelector } from 'react-redux';
-jest.mock('react-redux');
 
 describe('RoundOne component', () => {
   it('should match the snapshot', () => {
-    useSelector.mockImplementation(() => false);
-    const wrapper = shallow(<RoundOne />);
-    expect(wrapper).toMatchSnapshot();
-  });
-
-  it('should match the snapshot if time is ended', () => {
-    useSelector.mockImplementation(() => true);
     const wrapper = shallow(<RoundOne />);
     expect(wrapper).toMatchSnapshot();
   });
--- src/Containers/RoundOne/RoundOne.tsx
+++ src/Containers/RoundOne/RoundOne.tsx
@@ -1,19 +1,13 @@
 import './RoundOne.scss';
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { AppStore } from 'interfaces';
 import Header from 'Components/Header/Header';
 import GameBoard from 'Containers/GameBoard/GameBoard';
 import ChosenWordTrain from 'Containers/ChosenWordTrain/ChosenWordTrain';
 // import Timer from 'Components/Timer/Timer';
 
 const RoundOne:React.FC = () => {
-  const timeEnded = useSelector((state: AppStore) => state.timeEnded);
-  
   return (
     <main className="round-one-board">
-      { timeEnded && <Redirect to='/round-two' /> }
       <Header />
       <GameBoard />
       <footer>
