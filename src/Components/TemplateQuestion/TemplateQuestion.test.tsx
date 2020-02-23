55c23b805da56408d2d8aa5099938fcd4f0874cd
James Miller <james.rex.miller.4@gmail.com>
2020-02-23 13:03:31 -0700

    Write snapshot test for TemplateQuestion component

---
+++ src/Components/TemplateQuestion/TemplateQuestion.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TemplateQuestion from './TemplateQuestion';
+import { mount } from 'enzyme';
+
+describe('TemplateQuestion Component', () => {
+  it('should match the snapshot', () => {
+    const wrapper = mount(<TemplateQuestion 
+      templateQuestion={'A neighbor sings at a most ungodly hour'}/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
