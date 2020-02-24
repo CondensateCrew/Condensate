d5e254ac9356c0d1a84ebc1c05cb91ef65b0b8fc
James Miller <james.rex.miller.4@gmail.com>
2020-02-24 12:43:32 -0700

    Add showNextInsight method and functionality

--- src/Components/DisplayInsights/DisplayInsights.tsx
+++ src/Components/DisplayInsights/DisplayInsights.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { addInsight } from 'redux/actions';
+import { AppStore, Insight } from 'interfaces';
+import mockInsights from 'data/mockInsights';
 import './DisplayInsights.scss';
 import down from 'assets/down.svg';
 
 const DisplayInsights:React.FC = () => {
+  const [ currentStep, setCurrentStep ] = useState<number>(0)
+  const [ insights, setInsights ] = useState<Insight[]>(mockInsights)
+  
+  // const insights = useSelector((state: AppStore) => state.insights)
+
+  const showNextInsight = ():void => {
+    if (currentStep > 1) {
+      setCurrentStep(0)
+    } else {
+      setCurrentStep(currentStep + 1)
+    }
+  }
+
   return (
     <section className='display-insights-section'>
       <div className='display-insights-header-div'>
@@ -10,12 +27,12 @@ const DisplayInsights:React.FC = () => {
         <img src={down} alt='down-arrow'/>
       </div>
       <div className='display-insights-body-div'>
-        <h2>Some shit question...?</h2>
-        <li>Bears</li>
-        <li>Beers</li>
-        <li>Balalaika</li>
+        <h2>{insights[currentStep].question}</h2>
+        <li>{insights[currentStep].answers[0]}</li>
+        <li>{insights[currentStep].answers[1]}</li>
+        <li>{insights[currentStep].answers[2]}</li>
       </div>
-      <button>Next</button>
+      <button type='button' onClick={showNextInsight}>Next</button>
     </section>
   )
 }
