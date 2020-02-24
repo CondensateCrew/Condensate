3f5c329be3d2ea4308a1f5b078763afaea53fc87
Raisa Primerova <riyuhide@gmail.com>
2020-02-24 13:11:43 -0700

    Update RoundOne component tests

--- src/Containers/RoundOne/RoundOne.test.tsx
+++ src/Containers/RoundOne/RoundOne.test.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import RoundOne from './RoundOne';
 import { shallow } from 'enzyme';
 
+let mockValue: boolean;
+jest.mock('react-redux', () => ({
+  useSelector: () => mockValue
+}));
+
 describe('RoundOne component', () => {
   it('should match the snapshot', () => {
+    mockValue = false;
+    const wrapper = shallow(<RoundOne />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot if time is ended', () => {
+    mockValue = true;
     const wrapper = shallow(<RoundOne />);
     expect(wrapper).toMatchSnapshot();
   });
