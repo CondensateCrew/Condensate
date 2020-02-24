c5d13db1de2ddbffcdc9866a810084b2a31e6181
James Miller <james.rex.miller.4@gmail.com>
2020-02-23 23:51:45 -0700

    Add DisplayInsights component + add html and styles

---
+++ src/Components/DisplayInsights/DisplayInsights.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './DisplayInsights.scss';
+import down from '../../assets/down.svg';
+
+const DisplayInsights:React.FC = () => {
+  return (
+    <section className='display-insights-section'>
+      <div className='display-insights-header-div'>
+        <h3>Insights</h3>
+        <img src={down} alt='down-arrow'/>
+      </div>
+      <div className='display-insights-body-div'>
+        <h2>Some shit question...?</h2>
+        <li>Bears</li>
+        <li>Beers</li>
+        <li>Balalaika</li>
+      </div>
+      <button>Next</button>
+    </section>
+  )
+}
+
+export default DisplayInsights;
