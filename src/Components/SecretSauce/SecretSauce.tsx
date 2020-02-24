0fbe469d5661e35523bb87d7e72d4c7386c57f0e
James Miller <james.rex.miller.4@gmail.com>
2020-02-24 11:34:48 -0700

    Add refereshRandomWord function and connect SecretSauce to the store

--- src/Components/SecretSauce/SecretSauce.tsx
+++ src/Components/SecretSauce/SecretSauce.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './SecretSauce.scss';
 import down from '../../assets/down.svg';
 import refresh from '../../assets/refresh.svg';
+import mockRandomWords from 'data/mockRandomWords';
+import { useSelector, useDispatch } from 'react-redux';
+import { AppStore } from 'interfaces';
+import { addSecretSauce } from 'redux/actions';
 
 const SecretSauce:React.FC = () => {
+  const [ currentStep, setCurrentStep ] = useState<number>(0)
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(addSecretSauce(mockRandomWords))
+  }, [dispatch])
+
+  const secretSauce  = useSelector((state:AppStore) => state.secretSauce)
+
+  const refreshSecretSauce = ():void => {
+    if (currentStep < 9) {
+      setCurrentStep(currentStep + 1)
+    } else {
+      setCurrentStep(0)
+    }
+  }
+
   return (
     <section className='secret-sauce-section'>
       <div className='secret-sauce-header-div'>
@@ -11,8 +33,8 @@ const SecretSauce:React.FC = () => {
         <img src={down} alt='down-arrow'/>
       </div>
       <div className='secret-sauce-display-div'>
-        <h2>Goose</h2>
-        <img src={refresh} alt='refresh-recycle icon'/>
+        <h2>{secretSauce[currentStep]}</h2>
+        <img onClick={refreshSecretSauce} src={refresh} alt='refresh-recycle icon'/>
       </div>
     </section>
   )
