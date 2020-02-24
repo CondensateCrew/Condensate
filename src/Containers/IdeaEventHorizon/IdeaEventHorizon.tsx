7f8dd08d3db6589dfc2f4b872d392bbe460a5810
James Miller <james.rex.miller.4@gmail.com>
2020-02-23 21:49:47 -0700

    Add Heaheader to IdeaEventHorizon

--- src/Containers/IdeaEventHorizon/IdeaEventHorizon.tsx
+++ src/Containers/IdeaEventHorizon/IdeaEventHorizon.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Header from '../../Components/Header/Header';
 
 const IdeaEventHorizon:React.FC = () => {
   return (
-    <section>
+    <main>
+      <Header />
       <h2>This is the final round container where the user is prompted to revisit
         their original question.
       </h2>
-    </section>
+    </main>
   )
 }
 
