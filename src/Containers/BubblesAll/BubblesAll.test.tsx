1c46d7e7be05c7d47317096b2e0e9e8e297c85e1
Raisa Primerova <riyuhide@gmail.com>
2020-02-24 14:04:56 -0700

    Test BubblesAll component functionality

---
+++ src/Containers/BubblesAll/BubblesAll.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import BubblesAll from './BubblesAll';
+import { shallow } from 'enzyme';
+const mockDispatch = jest.fn();
+
+let mockTime: boolean;
+
+jest.mock('react-redux', () => ({
+  useDispatch: () => mockDispatch,
+  useSelector: () => mockTime
+}));
+
+describe('BubblesAll component', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should match the snapshot', () => {
+    mockTime = false;
+    const wrapper = shallow(
+      <BubblesAll />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot if time is ended', () => {
+    mockTime = true;
+    const wrapper = shallow(
+      <BubblesAll />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
