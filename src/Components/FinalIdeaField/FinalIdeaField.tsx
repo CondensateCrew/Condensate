5db39087db3f5228d4f50112bcf630fd63ddbbfb
James Miller <james.rex.miller.4@gmail.com>
2020-02-24 14:50:18 -0700

    Add useSelector functionality for displaying Original Question

--- src/Components/FinalIdeaField/FinalIdeaField.tsx
+++ src/Components/FinalIdeaField/FinalIdeaField.tsx
@@ -1,23 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { addCurrentBrainstorm } from 'redux/actions';
+import { AppStore } from 'interfaces';
+import mockCurrentBrainstorm from 'data/mockCurrentBrainstorm';
 import './FinalIdeaField.scss';
 import edit from '../../assets/edit.svg';
 
 const FinalIdeaField:React.FC = () => {
   const [ brainstormIdea, setBrainstormIdea ] = useState<string>('');
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(addCurrentBrainstorm(mockCurrentBrainstorm))
+  }, [dispatch])
+
+const originalQuestion = useSelector((state:AppStore) => state.currentBrainstorm.question)
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setBrainstormIdea(e.target.value)
   }
 
+
   const postBrainstormIdea = () => {
     // apiCall to POST final idea to the server, then redirect back to dashboard
   }
-  
+
   return (
     <section className='final-idea-field-section'>
       <div className='final-idea-question-div'>
-        <h3>Original Question that was asked?</h3>
+        <h3>{originalQuestion}</h3>
         <img src={edit} alt='pencil'/>
       </div>
       <div className='final-idea-field-div'>
---
+++ src/data/mockCurrentBrainstorm.ts
@@ -0,0 +1,10 @@
+const mockCurrentBrainstorm = {
+  id: 0,
+  question: 'How can we save the environment?',
+  idea: '',
+  action: 'Create an App',
+  isGenius: false,
+  categories: [{id: 0, name: 'Environment'}]
+}
+
+export default mockCurrentBrainstorm;
