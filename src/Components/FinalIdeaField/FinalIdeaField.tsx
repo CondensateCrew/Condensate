6f0c3f3e6b54c799fd7914400dbb5bb306340e1b
James Miller <james.rex.miller.4@gmail.com>
2020-02-23 23:08:12 -0700

    Add html and styles for SecretSauce component

--- src/Components/FinalIdeaField/FinalIdeaField.tsx
+++ src/Components/FinalIdeaField/FinalIdeaField.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './FinalIdeaField.scss';
+import edit from '../../assets/edit.svg';
 
 const FinalIdeaField:React.FC = () => {
   return (
     <section className='final-idea-field-section'>
       <div className='final-idea-question-div'>
         <h3>Original Question that was asked?</h3>
+        <img src={edit} alt='pencil'/>
       </div>
       <div className='final-idea-field-div'>
         <textarea placeholder="Type your idea here..."></textarea>
--- src/Components/SecretSauce/SecretSauce.tsx
+++ src/Components/SecretSauce/SecretSauce.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './SecretSauce.scss';
+import down from '../../assets/down.svg';
+import refresh from '../../assets/refresh.svg';
 
 const SecretSauce:React.FC = () => {
   return (
-    <h2>I am a randomly generated word,
-      with a button to get a new random word</h2>
+    <section className='secret-sauce-section'>
+      <div className='secret-sauce-header-div'>
+        <h3>Random Word</h3>
+        <img src={down} alt='down-arrow'/>
+      </div>
+      <div className='secret-sauce-display-div'>
+        <h2>Goose</h2>
+        <img src={refresh} alt='refresh-recycle icon'/>
+      </div>
+    </section>
   )
 }
 
