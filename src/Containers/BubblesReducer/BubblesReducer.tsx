71516ad38590957df1b53d19e3041f8f84980ed1
Raisa Primerova <riyuhide@gmail.com>
2020-02-24 13:59:39 -0700

    Add BubblesReducer component functinality

---
+++ src/Containers/BubblesReducer/BubblesReducer.tsx
@@ -0,0 +1,71 @@
+import React, {useState, useEffect} from 'react';
+import { Redirect } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addChosenWord, removeAllWords } from 'redux/actions';
+
+interface Props {
+  chosenWords: string[]
+}
+
+const BubblesReducer: React.FC<Props> = ({ chosenWords }) => {
+  const dispatch = useDispatch();
+  const [ isRedirected, setIsRedirected ] = useState<boolean>(false);
+  const [ isDisabled, setIsDisabled ] = useState<boolean>(false);
+  const [ count, setCount ] = useState<number>(0);
+  const [ bubbles, setBubbles ] = useState<string[]>([]);
+  const [ reducedBubbles, setReducedBubbles ] = useState<string[]>([]);
+
+  useEffect(() => {
+    setBubbles(chosenWords.slice(count, count + 3));
+    setIsDisabled(false);
+  }, [ chosenWords, count ]);
+
+  const goToNextTriple = (event: React.MouseEvent):void => {
+    const currentTarget = event.target as HTMLElement;
+    updateElements(currentTarget);
+    setIsDisabled(true);
+    setTimeout(() => {
+      setReducedBubbles([...reducedBubbles, currentTarget.id]);
+      setCount(count + 3);
+    }, 1500);
+  };
+
+  useEffect(() => {
+    if (reducedBubbles.length === 3) {
+      dispatch(removeAllWords());
+      reducedBubbles.map((bbl: string) => dispatch(addChosenWord(bbl)));
+      setIsRedirected(true);
+    }
+  }, [reducedBubbles])
+
+  const bubbleEls: JSX.Element[] = bubbles.map((bubble: string, i: number) => (
+    <p
+      key={`${bubble}-${i + 1}`}
+      className="bubble">
+      <span></span>
+      <button id={bubble.toLowerCase()} onClick={goToNextTriple} disabled={isDisabled}>{bubble}</button>
+    </p>
+  ));
+
+  return (
+    <section className="limited-bbls">
+      <h3>Choose one</h3>
+      <section>
+        {!isRedirected
+          ? bubbleEls
+          : <Redirect to='/round-two' />
+        }
+      </section>
+    </section>
+  )
+}
+
+export const updateElements = (currentTarget: HTMLElement): void => {
+  const parent = currentTarget.parentNode as HTMLElement;
+  const sibling = currentTarget.previousSibling as HTMLElement;
+  parent.classList.add('disappeared');
+  sibling.classList.add('bubble-rounds');
+  currentTarget.style.animation = 'disappearing 10s';
+}
+
+export default BubblesReducer;
