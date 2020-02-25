720acd5a2a87d7d4fb5fc749bfc5d8ba720b88c0
Raisa Primerova <riyuhide@gmail.com>
2020-02-25 12:40:33 -0700

    Recreate structure of ActionField componet

--- src/Containers/ActionsField/ActionsField.tsx
+++ src/Containers/ActionsField/ActionsField.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import BrainstormAction from '../../Components/BrainstormAction/BrainstormAction';
 import mockActions from '../../data/mockActions';
 import './ActionsField.scss';
+import down from 'assets/down.svg';
 
 import { IBrainstormForm } from '../../interfaces';
 
@@ -11,27 +12,42 @@ interface Props {
 }
 
 const ActionsField:React.FC<Props> = ({ formState, setAction }) => {
-  const [ selectedAction, setSelectedAction ] = useState<string>('create an app')
+  const [ selectedAction, setSelectedAction ] = useState<string>('Create an app');
+  const [ isClicked, setIsClicked ] = useState<boolean>(false);
 
-  const handleChange = (e:React.ChangeEvent<HTMLSelectElement>):void => {
-    setSelectedAction(e.target.value)
+  const handleChange = (e:React.MouseEvent<HTMLElement>):void => {
+    const currentTarget = e.target as HTMLElement;
+    setSelectedAction(currentTarget.innerText);
+    setIsClicked(false);
   }
+
+  const toggleBlock = (): void => setIsClicked(!isClicked);
 
   const updateAction = () => {
     setAction({...formState, action: selectedAction})
   }
 
-  useEffect(updateAction, [selectedAction])
+  useEffect(updateAction, [ selectedAction ]);
 
-  const actions = mockActions.map((action:string, index:number) => <BrainstormAction 
-  key={index} action={action}/>)
+  const actions = mockActions.map((action:string, index:number) => {
+    if (selectedAction.toLowerCase() !== action.toLowerCase()) {
+      return <li key={index} onClick={handleChange}>{action}</li>
+    }
+  });
 
+  const headerState = (isClicked) ? 'clicked' : '';
 
   return (
-    <div>
-      <select value={selectedAction} onChange={handleChange} className='actions-dropdown-menu'>
-        {actions}
-      </select>
+    <div className="action-toggle">
+      <header className={headerState} onClick={toggleBlock}>
+        <p className="chosen">{selectedAction}</p>
+        <img src={down} alt="down"/>
+      </header>
+      {isClicked &&
+        <ul className={headerState}>
+        { actions }
+        </ul>
+      }
     </div>
   )
 }
