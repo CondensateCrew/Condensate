0c9a953e2c689836291abaa80500e33e14c36d6e
Raisa Primerova <riyuhide@gmail.com>
2020-02-24 20:41:56 -0700

    Refactor UserProfile code

--- src/Components/UserProfile/UserProfile.tsx
+++ src/Components/UserProfile/UserProfile.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { AppStore } from 'interfaces';
@@ -10,12 +11,41 @@ const UserProfile:React.FC = () => {
     firstName: state.user.firstName,
     lastName: state.user.lastName
   }))
+=======
+import './UserProfile.scss';
+import React, { useState } from 'react';
+import AlanBird from '../../assets/avatar.jpg';
+import menu from '../../assets/menu-dot.svg';
+import close from '../../assets/close.svg';
+import logout from '../../assets/logout.svg';
+import { logOutUser } from 'redux/actions';
+import { useDispatch } from 'react-redux';
+
+interface Props {
+  firstName: string,
+  lastName: string,
+}
+
+const UserProfile:React.FC<Props> = ({ firstName, lastName }) => {
+  const dispatch = useDispatch();
+  const [ isClicked, setIsClicked ] = useState<boolean>(false);
+
+  const toggleMenu = (): void => setIsClicked(!isClicked);
+
+  const logoutUser = () => dispatch(logOutUser());
+>>>>>>> Refactor UserProfile code
 
   return (
     <div className='user-profile-div'>
       <img className='profile-pic' src={AlanBird} alt='user profile'/>
-      <h2><span>{firstName}</span> <span>{lastName}</span></h2>
-      <img className='menu-svg' src={menu} alt='menu-svg' />
+      <h2>{`${firstName} ${lastName}`}</h2>
+      {!isClicked
+        ? <img className='menu-svg' src={menu} alt='menu-svg' onClick={toggleMenu} />
+        : <section>
+          <img className='logout' src={logout} alt='logout' onClick={logoutUser}/>
+          <img className='close' src={close} alt='close' onClick={toggleMenu} />
+        </section>
+      }
     </div>
   )
 }
