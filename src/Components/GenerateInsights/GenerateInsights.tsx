3645f6add9d0cfa22205f9e01fd370b89f1a279a
Raisa Primerova <riyuhide@gmail.com>
2020-02-25 22:42:55 -0700

    Add test for GenerateInsights component

--- src/Components/GenerateInsights/GenerateInsights.tsx
+++ src/Components/GenerateInsights/GenerateInsights.tsx
@@ -5,21 +5,20 @@ import './GenerateInsights.scss';
 interface Props {
   text: string,
   responses: string[],
-  setResponses: (responses: string[]) => void
-  key:number,
+  setResponses: (responses: string[]) => void,
   id:number
 }
 
-const GenerateInsights:React.FC<Props> = ({ text, responses, setResponses, key, id }) => {
+const GenerateInsights:React.FC<Props> = ({ text, responses, setResponses, id }) => {
 
   const removeResponse = (e: React.SyntheticEvent<HTMLElement>):void => {
     let newResponses = responses;
-    newResponses.splice(id, 1)
-    setResponses([...newResponses])
-  }
+    newResponses.splice(id, 1);
+    setResponses([...newResponses]);
+  };
 
   return (
-    <div className='insights-div' key={key} id={id.toString()}>
+    <div className='insights-div' key={id} id={id.toString()}>
       <h3>{text}</h3>
       <img onClick={removeResponse} src={Close} alt='delete icon' />
     </div>
