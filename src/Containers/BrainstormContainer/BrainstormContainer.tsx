eeff599a8fbd4e7e61f69b136bb6362d46737b9b
Raisa Primerova <riyuhide@gmail.com>
2020-03-12 00:22:59 -0600

    Add semicolons and types for variables and functions to containers

--- src/Containers/BrainstormContainer/BrainstormContainer.tsx
+++ src/Containers/BrainstormContainer/BrainstormContainer.tsx
@@ -1,7 +1,7 @@
 import './BrainstormContainer.scss';
 import React from 'react';
 import { useSelector } from "react-redux";
-import { Brainstorm, AppStore } from 'interfaces';
+import { Brainstorm, AppStore, Category } from 'interfaces';
 import BrainstormCard from 'Components/BrainstormCard/BrainstormCard';
 
 const BrainstormContainer:React.FC = () => {
@@ -11,11 +11,11 @@ const BrainstormContainer:React.FC = () => {
     filterValue: state.filter
   }));
 
-  const filteredBrainstorms: (Brainstorm | undefined)[] = allBrainstorms
+  const filteredBrainstorms: Brainstorm[] = allBrainstorms
     .filter((bs: Brainstorm) => (
       ((filterValue === 'genius' && bs.isGenius === true)
       ||
-      bs.categories.find(ctg => {
+      bs.categories.find((ctg: Category) => {
         return (filterValue === '')
           ? ctg.name !== filterValue
           : ctg.name === filterValue
@@ -25,18 +25,15 @@ const BrainstormContainer:React.FC = () => {
       || bs.action.action.toLowerCase().includes(queryValue.toLowerCase()))
     ));
 
-  const cards:JSX.Element[] = filteredBrainstorms.map((bs:(Brainstorm | undefined) ):any => {
-    if (bs) {
-      return <BrainstormCard key={`bs-${bs.id}`} {...bs} />
-    }
-    return undefined
-  });
+  const cards:JSX.Element[] = filteredBrainstorms.map((bs:Brainstorm) => (
+    <BrainstormCard key={`bs-${bs.id}`} {...bs} />
+  ));
 
   return (
     <section className="bs-container">
       { cards }
     </section>
-  )
+  );
 }
 
 export default BrainstormContainer;
--- src/Containers/MenuBlock/MenuBlock.tsx
+++ src/Containers/MenuBlock/MenuBlock.tsx
@@ -5,24 +5,25 @@ import { toggleBrainstorm, deleteBrainstorm} from 'redux/actions'
 import ideaIcon from 'assets/idea.svg';
 import closeIcon from 'assets/close.svg';
 import deleteIcon from 'assets/delete.svg';
+import {IChangeBrainstormAction, IDeleteBrainstormAction } from 'interfaces';
 
 interface Props {
   id: number,
-  setIsClicked: (isClicked: boolean) => void;
+  setIsClicked: (isClicked: boolean) => void
 };
 
 const MenuBlock: React.FC<Props> = ({setIsClicked, id}) => {
   const closeMenu = (): void => setIsClicked(false);
   const dispatch = useDispatch();
-  const toggleCard = () => dispatch(toggleBrainstorm(id));
-  const deleteCard = () => dispatch(deleteBrainstorm(id));
+  const toggleCard = (): IChangeBrainstormAction => dispatch(toggleBrainstorm(id));
+  const deleteCard = (): IDeleteBrainstormAction => dispatch(deleteBrainstorm(id));
   return (
     <div className="menu-block">
       <img src={ideaIcon} alt="idea" onClick={toggleCard}/>
       <img src={deleteIcon} alt="delete" onClick={deleteCard}/>
       <img src={closeIcon} alt="close" onClick={closeMenu}/>
     </div>
-  )
+  );
 }
 
 export default MenuBlock;
