56b4a7a809ade92e011025b946fda58e74273a5b
Raisa Primerova <riyuhide@gmail.com>
2020-03-14 17:53:05 -0600

    Adding cookies to Login and Sign Up functionality

--- src/Containers/SignUpForm/SignUpForm.tsx
+++ src/Containers/SignUpForm/SignUpForm.tsx
@@ -1,18 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { addUser, addWordSamples, addAllActions, addAllCategories, addSecretSauce } from 'redux/actions';
-import { UserSignupPosting, WordSample, UserLoginReceived } from '../../interfaces';
+import { UserSignupPosting } from '../../interfaces';
 import InputElement from '../../Components/InputElement/InputElement';
 import { validateCredentials } from '../../_utils';
-import { Redirect } from 'react-router-dom';
-import { postUser, getSetUp, getDashboard } from 'apiCalls/apiCalls';
+import { postUser } from 'apiCalls/apiCalls';
 import LoadingImage from 'Components/LoadingImage/LoadingImage';
 
 type checkedInputType = 'first-name' | 'last-name' | 'email' | 'password' | 'repeat-password';
 
 interface Props {
   isLogin: boolean,
-  toggleTab: (login: boolean) => void
+  toggleTab: (login: boolean) => void,
+  setCookie: (name: string, value: any, options?: object) => void
 };
 
 interface UserPosted {
@@ -22,7 +20,7 @@ interface UserPosted {
   email: string,
 }
 
-const SignUpForm: React.FC<Props> = ({ isLogin, toggleTab }) => {
+const SignUpForm: React.FC<Props> = ({ isLogin, toggleTab, setCookie }) => {
   const [ user, setUser ] = useState<UserSignupPosting>({
     email: '',
     password: '',
@@ -32,9 +30,7 @@ const SignUpForm: React.FC<Props> = ({ isLogin, toggleTab }) => {
   const [ disabled, setDisabled ] = useState<boolean>(true);
   const [ repeatPassword, setPassword ] = useState<string>('');
   const [ error, setError ] = useState<string>('');
-  const [ isLoaded, setIsLoaded ] = useState<boolean>(false);
   const [ isLoading, setIsLoading ] = useState<boolean>(false);
-  const dispatch = useDispatch();
 
   const validateButton = (): void => {
     if (user.email === '') return setDisabled(true);
@@ -62,59 +58,29 @@ const SignUpForm: React.FC<Props> = ({ isLogin, toggleTab }) => {
     event.preventDefault();
     validateInputs();
 
-    if (error === '') {
-      const token = await postNewUser();
-      await fetchGameStuff(token);
-      return error === '' && setIsLoaded(true);
-    }
+    if (error === '') await postNewUser();
   }
 
   const postNewUser = async () => {
     try {
+      const { firstName, lastName, password, email } = user;
       setDisabled(true);
       setIsLoading(true);
       const userNew: UserPosted = {
-        first_name: user.firstName,
-        last_name: user.lastName,
-        password: user.password,
-        email: user.email,
+        first_name: firstName,
+        last_name: lastName,
+        password: password,
+        email: email,
       };
-
       const { token, first_name, last_name} = await postUser(userNew);
-
-      const modifiedUser: UserLoginReceived = {
-        id: token,
-        firstName: first_name,
-        lastName: last_name
-      };
-
-      dispatch(addUser(modifiedUser));
-
-      return token;
+      setCookie('token', token, { path: '/' });
+      setCookie('name', `${first_name}-${last_name}`, { path: '/' });
     }
     catch (error) {
       setError(error.message);
       setIsLoading(false);
     }
   }
-
-  const fetchGameStuff = async (token: string) => {
-    try {
-      const setUpRes = await getSetUp(token);
-      dispatch(addWordSamples(setUpRes));
-      const dashRes = await getDashboard(token);
-      dispatch(addAllActions(dashRes.actions));
-      dispatch(addAllCategories(dashRes.categories));
-      const secretSauce = await setUpRes
-        .map((word: WordSample): string => word.word);
-      dispatch(addSecretSauce(secretSauce));
-      setIsLoading(false);
-    }
-    catch (error) {
-      setError(error.message);
-      setIsLoading(false);
-    }
-  };
 
   const toggleForm = (): void => toggleTab(!isLogin);
 
@@ -127,6 +93,7 @@ const SignUpForm: React.FC<Props> = ({ isLogin, toggleTab }) => {
   ));
 
   return (
+<<<<<<< HEAD
     isLoaded
       ? <Redirect to='/dashboard' />
       : (
@@ -148,6 +115,25 @@ const SignUpForm: React.FC<Props> = ({ isLogin, toggleTab }) => {
           </form>
         </div>
       )
+=======
+    <div className="sign-up-form">
+      <header>
+        <h2 className="active">Sign Up</h2>
+        <h2 className="hidden" onClick={toggleForm}>Login</h2>
+      </header>
+      <form>
+        {error !== '' && <p className="error-notification">{error}</p>}
+        {inputsElements}
+        <button disabled={disabled} onClick={submitUser}>
+        {
+          isLoading
+            ? <LoadingImage />
+            : 'Sign Up'
+        }
+        </button>
+      </form>
+    </div>
+>>>>>>> Adding cookies to Login and Sign Up functionality
   );
 }
 
