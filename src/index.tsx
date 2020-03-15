049c484045cef9e72be3028f54934ed6119da607
Raisa Primerova <riyuhide@gmail.com>
2020-03-15 15:11:32 -0600

    Replace App component to containers folder

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/reset.scss';
 import './css/index.scss';
-import App from './Components/App/App';
+import App from './Containers/App/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
