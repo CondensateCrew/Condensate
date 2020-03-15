0e798dbba0603c37a761dd85beac5ae416fc2053
Raisa Primerova <riyuhide@gmail.com>
2020-03-15 15:19:03 -0600

    Replace SecretSauce component to containers folder

--- src/Containers/IdeaEventHorizon/IdeaEventHorizon.tsx
+++ src/Containers/IdeaEventHorizon/IdeaEventHorizon.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './IdeaEventHorizon.scss';
 import Header from 'Containers/Header/Header';
 import FinalIdeaField from 'Containers/FinalIdeaField/FinalIdeaField';
-import SecretSauce from 'Components/SecretSauce/SecretSauce';
+import SecretSauce from 'Containers/SecretSauce/SecretSauce';
 import DisplayInsights from 'Containers/DisplayInsights/DisplayInsights';
 
 const IdeaEventHorizon:React.FC = () => {
