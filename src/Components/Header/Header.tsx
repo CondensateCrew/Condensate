40e5b2aaef02f1073ecda0308e860e3b9a187d1f
James Miller <james.rex.miller.4@gmail.com>
2020-03-14 18:35:56 -0600

    Add backToDashboard method and import actionCreators

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,16 +1,27 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { removeCurrentBrainstorm, reverseInstructions, removeAllWords, removeInsights } from 'redux/actions';
 import './Header.scss';
 import back from 'assets/down.svg';
 
 const Header: React.FC = () => {
   const [ isRedirected, setIsRedirected ] = useState<boolean>(false);
+  const dispatch = useDispatch();
   const redirect = ():void => setIsRedirected(true);
+  
+  const backToDashboard = async () => {
+    await redirect();
+    dispatch(removeCurrentBrainstorm());
+    dispatch(reverseInstructions());
+    dispatch(removeAllWords());
+    dispatch(removeInsights());
+  };
 
   return (
     <header className="round-header">
       {isRedirected && <Redirect to="/dashboard" />}
-      <button onClick={redirect}>
+      <button onClick={backToDashboard}>
         <img src={back} alt="back"/>
         to dashboard
       </button>
