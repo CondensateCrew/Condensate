e623df955bb80a4b0cca702d2d1c71cf5d14f124
Raisa Primerova <riyuhide@gmail.com>
2020-03-15 23:59:11 -0600

    Update test for RoundTwo

--- src/Containers/RoundTwo/RoundTwo.test.tsx
+++ src/Containers/RoundTwo/RoundTwo.test.tsx
@@ -7,7 +7,6 @@ import RoundTwo from './RoundTwo';
 import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 
-
 const mockDispatch = jest.fn();
 jest.mock('react-redux', () => ({
   useSelector: () => ({
@@ -35,20 +34,23 @@ describe('RoundTwo Component', () => {
   });
 
   it('should match the snapshot when rendering for the first time', () => {
-    const wrapper = mount(<MemoryRouter
-      initialEntries={[ { pathname: '/', key: 'testKey' } ]}>
-        <RoundTwo /></MemoryRouter>)
+    const wrapper = mount(
+      <MemoryRouter initialEntries={[{ pathname: '/', key: 'testKey' }]}>
+        <RoundTwo />
+      </MemoryRouter>
+    );
 
     expect(wrapper).toMatchSnapshot();
   });
   describe('Change Events', () => {
     it('should update state when change event occurs', () => {
       const mockEvent = {target: {value: 'What does the fox say?'}};
-      const wrapper = mount(<MemoryRouter
-        initialEntries={[ { pathname: '/', key: 'testKey' } ]}>
-          <RoundTwo /></MemoryRouter>);
+      const wrapper: any = mount(
+        <MemoryRouter initialEntries={[{ pathname: '/', key: 'testKey' }]}>
+          <RoundTwo />
+        </MemoryRouter>
+      );
 
-      const instance = wrapper.instance();
       wrapper.find('input').simulate('change', mockEvent);
       expect(wrapper.find('input').getDOMNode().value).toEqual('What does the fox say?')
     });
